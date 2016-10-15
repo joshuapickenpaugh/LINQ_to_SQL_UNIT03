b35ddf3ffeedf2408dc3d442723ec0ae61f296a6
joshuapickenpaugh <joshuapickenpaugh@hotmail.com>
2016-10-15 15:02:27 -0500

    Commit

--- QueryLINQSQL/Module1.vb
+++ QueryLINQSQL/Module1.vb
@@ -1,6 +1,14 @@
-﻿Module Module1
+﻿Imports System.Linq
+Imports System.Collections.Generic
+Imports System.Data.Linq
+Imports System.Data.Linq.Mapping
+
+Module Module1
 
     Sub Main()
+
+        Dim cse_dept_typed_in As New CSE_DEPT_TYPED_INDataContext()
+
 
     End Sub
 
